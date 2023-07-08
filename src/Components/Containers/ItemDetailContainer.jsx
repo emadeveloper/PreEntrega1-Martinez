9166f95ae88d5eaf8c4c2390b36154bdfd00579b
Emanuel Martinez <yinproducciones@gmail.com>
2023-07-08 20:35:58 -0300

    fixing product not found

--- src/Components/Containers/ItemDetailContainer.jsx
+++ src/Components/Containers/ItemDetailContainer.jsx
@@ -3,26 +3,31 @@ import { useState, useEffect } from "react";
 import { getProductFromFirestore } from "../services/firebase/products";
 import { useParams } from "react-router-dom";
 import { Spinner } from "react-bootstrap";
+import "./container-styles/item-detail-container.css";
 
 const ItemDetailContainer = () => {
   const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { itemId } = useParams();
 
   useEffect(() => {
     getProductFromFirestore(itemId).then((result) => {
       setProduct(result);
+      setLoading(false);
     });
   }, [itemId]);
 
   return (
     <div>
-      {!product ? (
+      {loading ? (
         <div className="loading-spinner">
           <Spinner className="spinner-custom" animation="border" role="status">
             <span className="visually-hidden">Cargando...</span>
             </Spinner>
             </div>
-            ) : <ItemDetail product={product} />
+            ) : product ? (
+              <ItemDetail product={product} />
+              ) : (<h2 className="item-not-found">El producto no existe</h2>)
             }
       
     </div>
